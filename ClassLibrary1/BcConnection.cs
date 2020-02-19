9d206ccf79777d0b6a417a5ae8ac6d62e36db207
Traci Chan <tlc668@gmail.com>
2020-02-19 18:57:07 -0500

    Add project files.

---
+++ ClassLibrary1/BcConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SqlLibrary {
+    public class BcConnection {
+        //property to hold sqlconnection
+        public SqlConnection Connection {get; set;}
+
+        //create and open up connection use connection string
+        public void Connect(string server, string database, string auth) {
+            var connStr = $"server={server};database={database};{auth}";
+            //create an instance of sqlconnection
+            Connection = new SqlConnection(connStr);
+            //do an open try to make connection
+            Connection.Open();
+            //check to see if open
+            if (Connection.State != System.Data.ConnectionState.Open) {
+                Console.WriteLine("Could not open the connection  check connection string");
+                Connection = null;
+                return;
+                }
+            Console.WriteLine(  "Connection open");
+            }
+        public void Disconnect() {
+            if(Connection == null) {
+                return;
+
+            }
+            if(Connection.State == System.Data.ConnectionState.Open) {
+                Connection.Close();
+                Connection.Dispose();
+                Connection = null;
+            }
+        }
+        
+    }
+}
---
+++ SqlLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SqlLibrary;
+
+namespace SqlLesson {
+    class Program {
+        static void Main(string[] args) {
+           
+
+            var sqllib = new BcConnection();
+            sqllib.Connect(@"localhost\sqlexpress", "EdDb", "trusted_connection=true");
+            var student = new Student(sqllib);
+            var students = student.GetAllStudents();
+
+            sqllib.Disconnect();
+        }
+    }
+}
